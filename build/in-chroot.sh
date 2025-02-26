ecc73dd2fb572c8141155be0fed6d897c9b63e37
ppetroskevicius <p.petroskevicius@gmail.com>
2025-02-26 11:09:47 +0900

    fix: typo

--- build/in-chroot.sh
+++ build/in-chroot.sh
@@ -13,7 +13,7 @@ done
 
 # replace /opt/tinybox with the git repo
 rm -rf /opt/tinybox
-git clone https://github.com/ppetroskevicius/tinybox /opt/tinybox
+git clone https://github.com/ppetroskevicius/tinyos /opt/tinybox
 
 source /etc/tinybox-release
 if [[ -n "$TINYBOX_DEV" ]]; then
